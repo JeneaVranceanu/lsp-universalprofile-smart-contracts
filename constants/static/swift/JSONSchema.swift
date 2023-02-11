b40a4f2d36843c5b25b88491a5829f5bdefc7630
Jenea Vranceanu <36865532+JeneaVranceanu@users.noreply.github.com>
2023-02-11 17:47:18 +0200

    chore: updated static JSONSchema swift code

--- constants/static/swift/JSONSchema.swift
+++ constants/static/swift/JSONSchema.swift
@@ -3,7 +3,7 @@
 //  UniversalProfile
 //
 //  Created by JeneaVranceanu.
-//  LUKSO Blockchain GmbH © 2021
+//  LUKSO Blockchain GmbH © 2023
 //
 
 import Foundation
@@ -29,60 +29,136 @@ public struct JSONSchema {
         /// A key that maps a word, to a grouping word to an address.
         case MappingWithGrouping = "MappingWithGrouping"
     }
-    
-    public enum ValueType {
-        /// The bytes are a UTF8 encoded string
+
+    public indirect enum ValueType {
+        /// Boolean as one bit of data.
+        case boolean
+        /// UTF8 encoded string.
         case string
-        /// The bytes are an 20 bytes address
+        /// 20 bytes address.
         case address
-        /// The bytes are a uint256
-        case uint256
-        /// The bytes are a 32 bytes
-        case bytes32
-        /// The bytes are a bytes
-        case bytes
-        ///  The bytes are a UTF8 encoded string array
-        case stringArray
-        ///  The bytes are an 20 bytes address array
-        case addressArray
-        ///  The bytes are a uint256 array
-        case uint256Array
-        ///  The bytes are a bytes array
-        case bytesArray
-        ///  The bytes are a N bytes
-        case bytesNArray(Int)
-        
-        var rawValue: String {
+        /// uint up to 256 bits. 256 is default.
+        case uint(_ bitCount: UInt64 = 256)
+        /// int up to 256 bits. 256 is default.
+        case int(_ bitCount: UInt64 = 256)
+        /// Fixed size bytes of size between 1 and 32
+        case bytes(_ bytesCount: UInt64 = 0)
+        /// Fixed  size array.
+        case array(_ type: ValueType, _ arraySize: UInt64 = 0)
+        /// Array of tuples where the type of 1 tuple is a combination of types stored in `tupleType`. Size of the array is fixed.
+        case compactBytesArray(_ type: ValueType, _ arraySize: UInt64 = 0)
+        /// Bytes chunk that combins in itself multiple types and thus values.
+        case tuple(_ types: [ValueType])
+
+        /// Returns the actual number of bytes required to represent the type.
+        /// `nil` if the type cannot be sized without knowing the value, e.g. dynamic bytes or string.
+        ///
+        /// Example:
+        ///  - boolean requires only 1 byte (since 1 byte is the minimum) to store `true` or `false` value;
+        ///  - address required 20 bytes;
+        ///  - uint256 requires 32 bytes while uint16 only 2.
+        public var bytesSize: UInt64? {
             switch self {
-                case .string:
-                    return "string"
-                case .address:
-                    return "address"
-                case .uint256:
-                    return "uint256"
-                case .bytes32:
-                    return "bytes32"
-                case .bytes:
-                    return "bytes"
-                case .stringArray:
-                    return "string[]"
-                case .addressArray:
-                    return "address[]"
-                case .uint256Array:
-                    return "uint256[]"
-                case .bytesArray:
-                    return "bytes[]"
-                case .bytesNArray(let count):
-                    return "bytes\(count)[]"
+            case .boolean:
+                return 1
+            case .string:
+                return nil
+            case .address:
+                return 20
+            case .uint(let bitCount):
+                return bitCount / 8
+            case .int(let bitCount):
+                return bitCount
+            case .bytes(let bytesCount):
+                return bytesCount == 0 ? nil : bytesCount
+            case let .array(type, arraySize):
+                guard arraySize > 0,
+                      let subTypeSize = type.bytesSize else { return nil }
+                // 32 bytes array offset = 0x0000000000000000000000000000000000000000000000000000000000000020
+                // 32 * arraySize - is the bytes count for all indices of the array
+                return 32 + 32 * arraySize + subTypeSize * arraySize
+            case let .compactBytesArray(type, arraySize):
+                guard arraySize > 0,
+                      let subTypeSize = type.bytesSize else { return nil }
+                // 2 * arraySize - is the bytes count for bytes that store size of each element
+                return 2 * arraySize + subTypeSize * arraySize
+            case .tuple(let types):
+                var totalSize: UInt64 = 0
+                for type in types {
+                    guard let bytesSize = type.bytesSize else { return nil }
+                    totalSize += bytesSize
+                }
+                return totalSize
+            }
+        }
+        
+        public var isArray: Bool {
+            switch self {
+            case .array, .compactBytesArray:
+                return true
+            default:
+                return false
+            }
+        }
+
+        /// Type that has nested types
+        public var isComplexType: Bool {
+            switch self {
+            case .array, .compactBytesArray, .tuple:
+                return true
+            default:
+                return false
+            }
+        }
+
+        public var nestedTypes: [ValueType]? {
+            switch self {
+            case let .array(type, _):
+                return [type]
+            case let .compactBytesArray(type, _):
+                return [type]
+            case let .tuple(types):
+                return types
+            default:
+                return nil
+            }
+        }
+        
+        public var rawValue: String {
+            switch self {
+            case .boolean:
+                return "boolean"
+            case .string:
+                return "string"
+            case .address:
+                return "address"
+            case .uint(let size):
+                return "uint\(size)"
+            case .int(let size):
+                return "int\(size)"
+            case .bytes(let size):
+                return size == 0 ? "bytes" : "bytes\(size)"
+            case .array(let type, let count):
+                if count == 0 {
+                    return "\(type.rawValue)[]"
+                }
+                return "\(type.rawValue)[\(count)]"
+            case .compactBytesArray(let type, let count):
+                if count == 0 {
+                    return "\(type.rawValue)[CompactBytesArray]"
+                }
+                return "\(type.rawValue)(\(count))[CompactBytesArray]"
+            case .tuple(let types):
+                return "(\(types.map { $0.rawValue }.joined(separator: ",")))"
             }
         }
     }
     
-    public enum ValueContent {
+    public indirect enum ValueContent {
         ///  The content are bytes.
         case Bytes
         ///  The content are bytes with length N.
-        case BytesN(Int)
+        case BytesN(UInt64)
         ///  The content is a number.
         case Number
         ///  The content is a UTF8 string.
@@ -102,33 +178,36 @@ public struct JSONSchema {
         /// If the value content are specific bytes, than the returned value is expected to equal those bytes.
         case SpecificBytes(String)
         case Mixed
+        case tuple(_ types: [ValueContent])
         
         public var rawValue: String {
             switch self {
-                case .Bytes:
-                    return "Bytes"
-                case .BytesN(let val):
-                    return "Bytes\(val)"
-                case .Number:
-                    return "Number"
-                case .String:
-                    return "String"
-                case .Address:
-                    return "Address"
-                case .Keccak256:
-                    return "Keccak256"
-                case .AssetURL:
-                    return "AssetURL"
-                case .JSONURL:
-                    return "JSONURL"
-                case .URL:
-                    return "URL"
-                case .Markdown:
-                    return "Markdown"
-                case .SpecificBytes(let specificBytes):
-                    return specificBytes
-                case .Mixed:
-                    return "Mixed"
+            case .Bytes:
+                return "Bytes"
+            case .BytesN(let val):
+                return "Bytes\(val)"
+            case .Number:
+                return "Number"
+            case .String:
+                return "String"
+            case .Address:
+                return "Address"
+            case .Keccak256:
+                return "Keccak256"
+            case .AssetURL:
+                return "AssetURL"
+            case .JSONURL:
+                return "JSONURL"
+            case .URL:
+                return "URL"
+            case .Markdown:
+                return "Markdown"
+            case .SpecificBytes(let specificBytes):
+                return specificBytes
+            case .Mixed:
+                return "Mixed"
+            case .tuple(let types):
+                return "(\(types.map { $0.rawValue }.joined(separator: ",")))"
             }
         }
     }
